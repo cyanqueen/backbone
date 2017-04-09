bfc163eb68af3bda3dee89a37d125857231f113b
Bian <326443154@qq.com>
2017-04-09 17:55:23 +0800

    add initializer

---
+++ backbone-springmvc/src/main/java/org/backbone/easyweb/springmvc/annotation/ScanIgnore.java
@@ -0,0 +1,12 @@
+package org.backbone.easyweb.springmvc.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * @author bianliang (04/09/2017)
+ */
+@Documented
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ScanIgnore {
+}
---
+++ backbone-springmvc/src/main/java/org/backbone/easyweb/springmvc/config/Global.java
@@ -0,0 +1,10 @@
+package org.backbone.easyweb.springmvc.config;
+
+/**
+ * @author bianliang (04/09/2017)
+ */
+public class Global {
+
+    public static final String MVC_CONFIG_LOCATION = "classpath:web.properties";
+    public static final String BEAN_NAME_ROOT_PROPS = "rootProps";
+}
