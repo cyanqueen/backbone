5b2bcd43aecf6f63f5de49133a727c9b3e0e4c9a
bianliang <liang_bian@kingdee.com>
2017-07-12 16:45:41 +0800

    sharding

--- backbone-orm/src/main/java/org/backbone/orm/sharding/Sharding.java
+++ backbone-orm/src/main/java/org/backbone/orm/sharding/Sharding.java
@@ -4,6 +4,8 @@ package org.backbone.orm.sharding;
  * @author bianliang (05/14/2017)
  */
 public interface Sharding {
+
     String getDatabaseName(Object o);
+
     String getTableName(Object o);
 }
